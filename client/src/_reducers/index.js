b3f9ce95149f3d2404b5e0a00e3b4f597027c0ff
yuri <yulyuleee@gmail.com>
2020-07-19 11:52:42 +0900

    로그인 페이지

--- client/src/_reducers/index.js
+++ client/src/_reducers/index.js
@@ -1,10 +1,9 @@
 import { combineReducers } from 'redux';
-//import user from './user_reducer';
+import user from './user_reducer';
 
 // 여러 reducer들을 rootReducer에 하나로 합쳐줌
 const rootReducer = combineReducers({
-    //user,
-
+    user
 })
 
 export default rootReducer;
--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
-import { applyMiddleware } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
 import promiseMiddleware from 'redux-promise';
 import ReduxThunk from 'redux-thunk';
 import Reducer from './_reducers';
