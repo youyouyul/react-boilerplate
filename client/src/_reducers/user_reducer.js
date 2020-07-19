cccae2238a29b12595d717351f76ce40bc4a8016
yuri <yulyuleee@gmail.com>
2020-07-19 13:05:09 +0900

    인증처리

--- client/src/_reducers/user_reducer.js
+++ client/src/_reducers/user_reducer.js
@@ -1,5 +1,5 @@
 import {
-    LOGIN_USER, REGISTER_USER
+    LOGIN_USER, REGISTER_USER, AUTH_USER
 } from '../_actions/types';
 
 export default function (state = {}, action) {
@@ -9,6 +9,9 @@ export default function (state = {}, action) {
             break;
         case REGISTER_USER:
             return {...state, register: action.payload}
+            break;    
+        case AUTH_USER:
+            return{...state, userData: action.payload}
             break;    
         default:
             return state;    
